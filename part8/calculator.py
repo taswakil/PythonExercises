eaff4e0d5622a460f0cd1e4f33a80cf8ede99574
taswakil <designerfairytasneem@gmail.com>
2024-11-11 14:06:29 -0600

    Update calculator.py

--- part8/calculator.py
+++ part8/calculator.py
@@ -1,4 +1,11 @@
 #### Write a program that:
-
+num1 = 5
+num2 = 10
         #Defines two numbers in variables, num1 and num2.
         #Prints the result of adding, subtracting, multiplying, and dividing the two numbers.
+
+if num1 != num2:
+        print(num1 + num2)
+        print(num1 - num2)
+        print(num1 * num2)
+        print(num1 / num2)
