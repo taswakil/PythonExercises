4e34a6deca6eb95f26d98e01246085c853fc9ad5
taswakil <designerfairytasneem@gmail.com>
2024-11-11 14:06:10 -0600

    Update temperature_converter.py

--- part10/temperature_converter.py
+++ part10/temperature_converter.py
@@ -3,3 +3,8 @@
     #Stores a temperature in Celsius in a variable.
     #Converts the temperature to Fahrenheit using the formula (C * 9/5) + 32.
     #Prints the converted temperature
+
+C = 20
+
+if C == 20:
+    print((C * 9/5) + 32)
