c0a0e8a4240cfd5438b518969e65f5df43d59401
taswakil <designerfairytasneem@gmail.com>
2024-11-11 14:06:31 -0600

    Update even_odd_checker.py

--- part9/even_odd_checker.py
+++ part9/even_odd_checker.py
@@ -2,3 +2,11 @@
 
     #Stores a number in a variable.
     #Checks if the number is even or odd and prints "The number is even" or "The number is odd" accordingly.
+
+number = 6
+
+if number % 2 == 0:
+    print("The number is even")
+
+else:
+    print("The number is odd")
